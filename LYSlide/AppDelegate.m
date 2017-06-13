c6e1bccb52cda6475babaa37fe80948fd473099f
ying <mac@localhost>
2017-06-13 17:14:59 +0800

    提交测试

--- LYSlide/AppDelegate.m
+++ LYSlide/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window=[[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.rootViewController=[[LYNavigationController alloc]initWithRootViewController:[[ViewController alloc] init]];
-    
+    //sdsfs
     return YES;
 }
 
